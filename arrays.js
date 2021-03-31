a75183a002e9ca1e9f5737008cd28bbefb4329b7
dekkersbd <bbart.d@gmail.com>
2021-03-31 11:07:45 +0200

    afmaken arrays

--- arrays.js
+++ arrays.js
@@ -31,7 +31,9 @@ console.log(colours[4], colours[1]);
 // 5b. Overschrijf één van de items in de array dusdanig dat de cijferreeks netjes doorloopt (je mag de declaratie op de vorige regel niet aanpassen).
 // 5c. Log de array in de terminal                                                          // geeft [ 21, 22, 23, 24, 25 ]
 // ==========================================
-
+const moreNumbers = [21, 22, 23, 25, 25];
+moreNumbers[3] = 24;
+console.log(moreNumbers);
 
 // ==========================================
 // 6a. Declareer een variabele met daarin een array met de waardes bladerdeeg, knoflook, spinazie
@@ -39,3 +41,6 @@ console.log(colours[4], colours[1]);
 // 6c. Log de de array in de terminal                                                       // geeft [ 'lasagne bladen', 'knoflook', 'spinazie' ]
 // 6d. Log de lengte van de array in de terminal                                            // geeft 3
 // ==========================================
+const ingredienten = ["bladerdeeg", "knoflook", "spinazie"];
+ingredienten[0] = "lasagne bladen";
+console.log(ingredienten, ingredienten.length);
